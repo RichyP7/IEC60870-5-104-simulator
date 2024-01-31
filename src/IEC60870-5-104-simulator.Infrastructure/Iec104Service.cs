5e9f6b61658a1b7464fa83bcb39939ac7b80b182
Richard Putz <14166202+RichyP7@users.noreply.github.com>
2024-01-31 17:31:06 +0100

    impelemnt command datapoints

--- src/IEC60870-5-104-simulator.Infrastructure/Iec104Service.cs
+++ src/IEC60870-5-104-simulator.Infrastructure/Iec104Service.cs
@@ -10,16 +10,18 @@ namespace IEC60870_5_104_simulator.Infrastructure
         private lib60870.CS104.Server server;
         private readonly ILogger<Iec104Service> logger;
         private Iec104DataPointConfiguration configuration;
+        private readonly IValueSimulatorFactory valueFactory;
         private List<InformationObject> objectsToSimulate;
 
         public IInformationObjectFactory factory { get; }
 
-        public Iec104Service(lib60870.CS104.Server server, IInformationObjectFactory factory, ILogger<Iec104Service> logger, Iec104DataPointConfiguration configuration)
+        public Iec104Service(lib60870.CS104.Server server, IInformationObjectFactory factory, ILogger<Iec104Service> logger, Iec104DataPointConfiguration configuration, IValueSimulatorFactory simulatorProfile)
         {
             this.server = server;
             this.factory = factory;
             this.logger = logger;
             this.configuration = configuration;
+            this.valueFactory = simulatorProfile;
             objectsToSimulate = new List<InformationObject>();
         }
 
@@ -27,6 +29,8 @@ namespace IEC60870_5_104_simulator.Infrastructure
         {
             this.configuration = configuration;
             SetupIecDataPointList(this.configuration);
+            server.SetASDUHandler(asduHandler, null);
+
             this.server.Start();
             return Task.CompletedTask;
         }
@@ -52,18 +56,33 @@ namespace IEC60870_5_104_simulator.Infrastructure
         }
 
         public Task SimulateValues()
+
         {
-            //Todo: Iterate for all types available
-            var myList = this.objectsToSimulate.Where(v => v.Type.Equals(TypeID.M_SP_NA_1));
+            valueFactory.SimulateValues(this.objectsToSimulate);
             ASDU newAsdu = CreateAsdu();
-            foreach (InformationObject typeddataPoints in myList)
+            foreach (InformationObject typeddataPoints in objectsToSimulate.Where(v => v.Type.Equals(TypeID.M_SP_NA_1)))
             {
                 newAsdu.AddInformationObject(typeddataPoints);
             }
-
             server.EnqueueASDU(newAsdu);
             logger.LogDebug("Enqeued {asdu} items", newAsdu.NumberOfElements);
             return Task.CompletedTask;
+        }
+        private bool asduHandler(object parameter, IMasterConnection connection, ASDU asdu)
+        {
+            if (asdu.TypeId == TypeID.C_RC_NA_1)
+            {
+                logger.LogDebug("StepCommandd");
+                StepCommand sc = (StepCommand)asdu.GetElement(0);
+                ASDU newAsdu = CreateAsdu();
+                asdu.Cot = CauseOfTransmission.ACTIVATION_TERMINATION;
+                asdu.IsNegative = false;
+                asdu.AddInformationObject(sc);
+                server.EnqueueASDU(asdu);
+
+                return true;
+            }
+            return false;
         }
 
         private ASDU CreateAsdu()
