e16b186cfbc909bce8e00fb0b436aa83f679b7bf
Richard Putz <14166202+RichyP7@users.noreply.github.com>
2024-01-31 23:46:33 +0100

    add first test configuration

--- src/IEC60870-5-104-simulator.Domain/Iec104DataPointConfiguration.cs
+++ src/IEC60870-5-104-simulator.Domain/Iec104DataPointConfiguration.cs
@@ -9,9 +9,11 @@ namespace IEC60870_5_104_simulator.Domain
     public class Iec104DataPointConfiguration
     {
         List<Iec104DataPoint> dataPoints;
+        List<Iec104CommandDataPoint> commandDataPoints;
         public Iec104DataPointConfiguration()
         {
             dataPoints = new List<Iec104DataPoint>();
+            commandDataPoints = new List<Iec104CommandDataPoint>(); 
             SetupHardcoded();
         }
 
@@ -21,7 +23,12 @@ namespace IEC60870_5_104_simulator.Domain
             dataPoints.Add(new Iec104DataPoint(1, Iec104DataTypes.M_SP_NA_1));
             dataPoints.Add(new Iec104DataPoint(101, Iec104DataTypes.M_SP_NA_1));
             dataPoints.Add(new Iec104DataPoint(3, Iec104DataTypes.M_DP_NA_1));
-            dataPoints.Add(new Iec104DataPoint(9, Iec104DataTypes.M_ME_NA_1));
+            Iec104DataPoint steppositionTest = new Iec104DataPoint(9, Iec104DataTypes.M_ST_NA_1);
+            dataPoints.Add(steppositionTest);
+
+            var firstcommand = new Iec104CommandDataPoint(10, Iec104DataTypes.C_RC_NA_1);
+            firstcommand.AssignResponseDataPoint(steppositionTest);
+            commandDataPoints.Add(new Iec104CommandDataPoint(10, Iec104DataTypes.C_RC_NA_1));
         }
 
         public List<Iec104DataPoint> GetDataPointList()
