02580b1d94186a47e9340246a238a242cd4fb8f6
Richard Putz <richard.putz@bearingpoint.com>
2024-04-16 12:43:43 +0200

    implement cyclic random

--- src/IEC60870-5-104-simulator.Infrastructure/RandomObjectFactory.cs
+++ src/IEC60870-5-104-simulator.Infrastructure/RandomObjectFactory.cs
@@ -10,7 +10,14 @@ namespace IEC60870_5_104_simulator.Infrastructure
 {
     public class RandomObjectFactory : IInformationObjectFactory
     {
-        //Todo: implement remaining types
+        private readonly IInformationObjectTemplate template;
+        private readonly Random random;
+
+        public RandomObjectFactory(IInformationObjectTemplate template)
+        {
+            this.template = template;
+            random = new();
+        }
 
         public InformationObject GetInformationObject(Iec104DataPoint responseDataPoint)
         {
@@ -18,38 +25,69 @@ namespace IEC60870_5_104_simulator.Infrastructure
             switch (responseDataPoint.Iec104DataType)
             {
                 case Iec104DataTypes.M_ST_NA_1:
-                    int stepValue = CreateAdjustedStepValue(responseDataPoint.Address);
-                    return new StepPositionInformation(oa, (int)stepValue, true, new QualityDescriptor());case Iec104DataTypes.M_DP_TA_1:
-                    DoublePointValue value_M_DP_TA_1 = CreateDoublePointValue();
-                    return new DoublePointWithCP24Time2a(oa, value_M_DP_TA_1, new QualityDescriptor(), GetCP24Now());
+                case Iec104DataTypes.M_ST_TA_1:
+                case Iec104DataTypes.M_ST_TB_1:
+                    int stepValue = CreateAdjustedStepValue();
+                    return template.GetStepposition(responseDataPoint.Address.ObjectAddress, new IecIntValueObject(stepValue), responseDataPoint.Iec104DataType);
+                case Iec104DataTypes.M_SP_NA_1:
+                case Iec104DataTypes.M_SP_TA_1:
+                case Iec104DataTypes.M_SP_TB_1:
+                    bool spaValue = CreateSinglePointValue();
+                    return template.GetSinglePoint(responseDataPoint.Address.ObjectAddress, new IecSinglePointValueObject(spaValue), responseDataPoint.Iec104DataType);
+                case Iec104DataTypes.M_DP_NA_1:
+                case Iec104DataTypes.M_DP_TA_1:
                 case Iec104DataTypes.M_DP_TB_1:
-                    DoublePointValue value_M_DP_TB_1 = CreateDoublePointValue();
-                    return new DoubleCommandWithCP56Time2a(oa, (int)value_M_DP_TB_1, false, 0, GetCP56Now());
+                    IecDoublePointValue value_M_DP_NA_1 = CreateDoublePointValue();
+                    return template.GetDoublePoint(responseDataPoint.Address.ObjectAddress, new IecDoublePointValueObject(value_M_DP_NA_1), responseDataPoint.Iec104DataType);
+                case Iec104DataTypes.M_ME_NB_1:
+                case Iec104DataTypes.M_ME_TB_1:
+                case Iec104DataTypes.M_ME_TE_1:
+                    int scaled = CreateMeasuredValueScaled();
+                    return template.GetMeasuredValueScaled(responseDataPoint.Address.ObjectAddress, new IecIntValueObject(scaled), responseDataPoint.Iec104DataType);
+                case Iec104DataTypes.M_ME_NC_1:
+                case Iec104DataTypes.M_ME_TC_1:
+                case Iec104DataTypes.M_ME_TF_1:
+                    float valueFloat = CreateRandomFloat();
+                    return template.GetMeasuredValueShort(responseDataPoint.Address.ObjectAddress, new IecValueFloatObject(valueFloat), responseDataPoint.Iec104DataType);
                 case Iec104DataTypes.M_ME_NA_1:
-                    return new MeasuredValueScaled(oa, Random.Shared.Next(), new QualityDescriptor());
+                case Iec104DataTypes.M_ME_TA_1:
+                case Iec104DataTypes.M_ME_ND_1:
+                    float valueNormalized = CreateRandomFloat();
+                    return template.GetMeasuredValueNormalized(responseDataPoint.Address.ObjectAddress, new IecValueFloatObject(valueNormalized), responseDataPoint.Iec104DataType);
                 default:
                     throw new NotImplementedException($"{responseDataPoint.Iec104DataType} is not implemented");
             }
         }
 
-        private DoublePointValue CreateDoublePointValue()
+        private float CreateRandomFloat()
         {
-            Random random = new();
-            return random.NextDouble() >= 0.5 ? DoublePointValue.OFF : DoublePointValue.ON;
-        }
-        private int CreateAdjustedStepValue(IecAddress address)
-        {
-            Random random = new();
-            return (int)random.NextInt64(1, 5);
-        }
-        private CP56Time2a GetCP56Now()
-        {
-            return new lib60870.CP56Time2a(DateTime.Now);
+            return NextFloat(random);
         }
 
-        private static CP24Time2a GetCP24Now()
+        private int CreateMeasuredValueScaled()
         {
-            return new lib60870.CP24Time2a(DateTime.Now.Minute, DateTime.Now.Second, DateTime.Now.Millisecond);
+            return random.Next();
+        }
+
+        private bool CreateSinglePointValue()
+        {
+            return random.NextDouble() >= 0.5;
+        }
+
+        private IecDoublePointValue CreateDoublePointValue()
+        {
+            return random.NextDouble() >= 0.5 ? IecDoublePointValue.OFF : IecDoublePointValue.ON;
+        }
+        private int CreateAdjustedStepValue()
+        {
+            return random.Next(-64, 63);
+        }
+        static float NextFloat(Random random)
+        {
+            double mantissa = (random.NextDouble() * 2.0) - 1.0;
+            // choose -149 instead of -126 to also generate subnormal floats (*)
+            double exponent = Math.Pow(2.0, random.Next(-126, 128));
+            return (float)(mantissa * exponent);
         }
     }
 }
